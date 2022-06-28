b161be7f1960a114280eff5071c25fe75743a39d
Maksim <dumkin_m@rambler.ru>
2022-06-28 12:43:00 +0300

    Added programm3

---
+++ Sim2C#/Sim2C#Three task/Program3.cs
@@ -0,0 +1,44 @@
+﻿void Week()
+{
+//программа по обработке номера дня находит рабочий день или нет    
+   int days ;
+   Console.Write("Enter number of day :");
+   days=Convert.ToInt32(Console.ReadLine());
+// yes is a working day
+    if (days==1) 
+   {
+        Console.Write("Manday -is a  working day");
+    }
+    if (days==2) 
+   {
+        Console.Write("Tuesday -is a  working day");
+    }
+    if (days==3) 
+   {
+         Console.Write("Wednesday -is a  working day");
+    }
+    if (days==4) 
+   {
+        Console.Write("Thursday -is a  working day");
+    }
+    if (days==5) 
+   {
+        Console.Write("Friday -is a  working day");
+    }
+//no -not working day    
+    if (days==6) 
+   {
+        Console.Write("Saturday -is a day off");
+    }
+    if (days==7) 
+   {
+        Console.Write("Sunday -s a day off");
+    }
+//error)    
+    if (days<=0 | days>=8) 
+   {
+        Console.Write("The day of the week is entered incorrectly");
+    }
+}
+Week();
+
