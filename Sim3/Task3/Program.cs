97c280a4d9254761433bb910f52d2b85d0f6cdaa
Maksim <dumkin_m@rambler.ru>
2022-06-29 22:40:34 +0300

    Task3

---
+++ Sim3/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+void Cube (int number)
+
+{
+    for (int i=1;i<=number;i++)
+    {
+        Console.Write(i*i*i+" ");
+    }
+    
+}
+
+
+Console.WriteLine("Введите число для продолжения ...");
+
+int rez=Convert.ToInt32(Console.ReadLine());
+
+Cube(rez);
+
